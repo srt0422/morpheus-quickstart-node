b72bbf79b99eb1e042afd5f75f9eb6f5824dce3f
Jon Fairbanks <fairplay89@gmail.com>
2025-03-18 12:48:00 -0700

    Only grab .env.example if .env does not exist

--- cloud/deploy-local.sh
+++ cloud/deploy-local.sh
@@ -4,10 +4,12 @@ set -e
 
 # Pull in the docker-compose template & variables
 wget "https://github.com/MORpheus-Software/NFA/blob/main/UniversalBuilder/lib/docker-compose.yaml"
-wget "https://github.com/MORpheus-Software/NFA/blob/main/UniversalBuilder/lib/.env.example"
 
 # Check if the env file exists, otherwise rename the example file
-[ ! -f .env ] && mv .env.example .env
+if [ ! -f .env ]; then
+    wget -O .env.example "https://github.com/MORpheus-Software/NFA/blob/main/UniversalBuilder/lib/.env.example"
+    mv .env.example .env
+fi
 
 # Stand up the Morpheus stack
 docker compose up -d
