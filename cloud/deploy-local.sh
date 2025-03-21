77b63bac3e4a752631fb87dd1ab32af4e3754410
Jon Fairbanks <fairplay89@gmail.com>
2025-03-20 18:34:04 -0700

    Set wget output file

--- cloud/deploy-local.sh
+++ cloud/deploy-local.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Pull in the docker-compose template & variables
-wget "https://github.com/MORpheus-Software/NFA/blob/main/UniversalBuilder/lib/docker-compose.yaml"
+wget -O docker-compose.yaml "https://github.com/MORpheus-Software/NFA/blob/main/UniversalBuilder/lib/docker-compose.yaml"
 
 # Check if the env file exists, otherwise rename the example file
 if [ ! -f .env ]; then
