013c898a3f08b14c48498a489918e58de19c03dc
Jon Fairbanks <fairplay89@gmail.com>
2025-03-21 11:27:39 -0700

    Fix merge conflict

--- cloud/deploy-local.sh
+++ cloud/deploy-local.sh
@@ -9,12 +9,12 @@ cd "$WORK_DIR"
 
 # Pull in the docker-compose template if not present
 if [ ! -f docker-compose.yaml ]; then
-    wget -O docker-compose.yaml "https://raw.githubusercontent.com/MORpheus-Software/NFA/main/UniversalBuilder/lib/docker-compose.yaml"
+    wget -O docker-compose.yaml "https://raw.githubusercontent.com/MORpheus-Software/NFA/refs/heads/main/UniversalBuilder/lib/docker-compose.yaml"
 fi
 
 # Check if the env file exists, otherwise rename the example file
 if [ ! -f .env ]; then
-    wget -O .env.example "https://raw.githubusercontent.com/MORpheus-Software/NFA/main/UniversalBuilder/lib/.env.example"
+    wget -O .env.example "https://raw.githubusercontent.com/MORpheus-Software/NFA/refs/heads/main/UniversalBuilder/lib/.env.example"
     mv .env.example .env
 fi
 
