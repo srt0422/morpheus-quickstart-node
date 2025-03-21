0236cd20502a749de6a551df868b69db95be22d6
Builder <builder@example.com>
2025-03-21 09:47:30 -0700

    update source url's for deploy-local dependencies

--- cloud/deploy-local.sh
+++ cloud/deploy-local.sh
@@ -4,12 +4,13 @@ set -e
 
 # Pull in the docker-compose template if not present
 if [ ! -f docker-compose.yaml ]; then
-    wget -O docker-compose.yaml "https://github.com/MORpheus-Software/NFA/blob/main/UniversalBuilder/lib/docker-compose.yaml"
+    wget -O docker-compose.yaml "https://raw.githubusercontent.com/MORpheus-Software/NFA/refs/heads/main/UniversalBuilder/lib/docker-compose.yaml"
 fi
+
 
 # Check if the env file exists, otherwise rename the example file
 if [ ! -f .env ]; then
-    wget -O .env.example "https://github.com/MORpheus-Software/NFA/blob/main/UniversalBuilder/lib/.env.example"
+    wget -O .env.example "https://raw.githubusercontent.com/MORpheus-Software/NFA/refs/heads/main/UniversalBuilder/lib/.env.example"
     mv .env.example .env
 fi
 
